b2c0f9b6c13bb6845e4ae6fb5955baefd55f9ad8
Saikat379 <rsaikat978@gmail.com>
2021-10-12 15:40:12 +0530

    First commit for new Branch

---
+++ src/test/java/day1/Newtest.java
@@ -0,0 +1,14 @@
+package day1;
+import org.testng.annotations.Test;
+
+public class Newtest {
+  @Test
+  public void f() {
+	  System.out.println("hello");
+  }
+  public void f1() {
+	  System.out.println("h1");
+  }
+}
+
+
